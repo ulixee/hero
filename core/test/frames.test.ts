d53da165d649163dcb724225a2ea43ce88d7eacc
blakebyrnes <blakebyrnes@gmail.com>
2020-07-07 16:06:27 -0400

    feat: flatten shared workspaces

--- core/test/frames.test.ts
+++ core/test/frames.test.ts
@@ -1,7 +1,7 @@
 import Core from '../index';
-import { Helpers } from '../../shared/testing';
+import { Helpers } from '../../testing';
 import DomEnv from '../lib/DomEnv';
-import { ITestKoaServer } from '../../shared/testing/helpers';
+import { ITestKoaServer } from '@secret-agent/shared-testing/helpers';
 import { InteractionCommand } from '../../core-interfaces/IInteractions';
 import { LocationStatus } from '../../core-interfaces/Location';
 
