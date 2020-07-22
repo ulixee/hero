520a912d50b935270b75f2d6ef1faf5fed796e85
blakebyrnes <blakebyrnes@gmail.com>
2020-07-22 11:00:52 -0400

    fix: packaging issues with mitm and replay

--- replay/index.ts
+++ replay/index.ts
@@ -1,29 +1,34 @@
 import ChildProcess from 'child_process';
 import { getBinaryPath, isBinaryInstalled } from './Utils';
+import Log from '@secret-agent/commons/Logger';
+
+const { log } = Log(module);
 
 const apiPath = require.resolve('@secret-agent/replay-api/start');
 
-const showLogs = !!process.env.SA_REPLAY_DEBUG;
-
-export default function replay(args: IReplayLaunchArgs) {
-  const { sessionsDataLocation, sessionName, id } = args;
-  const argArray = [
-    require.resolve('@secret-agent/replay-app'),
-    sessionsDataLocation,
-    sessionName,
-    id,
-    apiPath,
-  ];
-
-  let executable = 'node $(yarn bin electron)';
-  let needsShell = true;
+export default function replay(launchArgs: IReplayLaunchArgs) {
+  const { sessionsDataLocation, sessionName, id } = launchArgs;
+  const spawnArgs = ['', sessionsDataLocation, sessionName, id, apiPath];
 
   if (isBinaryInstalled()) {
-    needsShell = false;
-    executable = getBinaryPath();
+    return spawn(getBinaryPath(), spawnArgs);
   }
 
-  ChildProcess.spawn(executable, argArray, {
+  try {
+    // see if we can launch from monorepo
+    spawnArgs[0] = require.resolve('@secret-agent/replay-app');
+
+    spawn('node $(yarn bin electron)', spawnArgs, true);
+  } catch (err) {
+    log.info('Replay app not found');
+    return;
+  }
+}
+
+function spawn(appPath: string, args: string[], needsShell: boolean = false) {
+  const showLogs = !!process.env.SA_REPLAY_DEBUG;
+
+  ChildProcess.spawn(appPath, args, {
     detached: true,
     stdio: showLogs ? 'inherit' : 'ignore',
     shell: needsShell,
