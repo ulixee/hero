51ebb1107ff42f19a453a268c243b19c2d0f2644
blakebyrnes <blakebyrnes@gmail.com>
2020-08-24 17:48:48 -0400

    feat(emulators): add windows runtime polyfills

--- jest.teardown.js
+++ jest.teardown.js
@@ -5,7 +5,7 @@ const { execSync } = require('child_process');
 module.exports = async () => {
   try {
     if (os.platform() === 'win32') {
-      execSync(`TASKKILL /IM chrome.exe /F`);
+      execSync(`taskkill /t /f /im chrome.exe 2> nul`);
     }
     fs.rmdirSync(`${__dirname}/.cache-test`, { recursive: true });
   } catch (err) {}
