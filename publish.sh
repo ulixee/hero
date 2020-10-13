62d1ef046bccb2d90df206d5425999a80b7d4fd8
blakebyrnes <blakebyrnes@gmail.com>
2020-10-13 11:41:46 -0400

    fix: publish latest replay

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-lerna version --conventional-commits --no-push --exact
+lerna version --conventional-commits --no-push --exact --conventional-graduate=replay
 yarn build:dist
 cd build-dist/
 npx lerna publish from-package
