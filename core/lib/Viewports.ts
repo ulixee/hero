66c1f4a89a434352ae5c8add46481c1f6d28f03f
Caleb Clark <calebjclark@gmail.com>
2020-11-26 10:04:36 -0600

    feat: randomize the Viewports browser positionX/Y

--- core/lib/Viewports.ts
+++ core/lib/Viewports.ts
@@ -1,19 +1,34 @@
 import IViewport from '@secret-agent/core-interfaces/IViewport';
-import IWindowFraming from "../../core-interfaces/IWindowFraming";
+import IWindowFraming from '../../core-interfaces/IWindowFraming';
 
 export default class Viewports {
-  static getDefault(windowFraming: IWindowFraming, base: IWindowFraming, ) {
+  static getDefault(windowFraming: IWindowFraming, base: IWindowFraming) {
     const screenWidth = 1440;
     const screenHeight = 900;
 
-    const positionX = windowFraming.screenGapLeft;
-    const positionY = windowFraming.screenGapTop;
-
-    const windowInnerWidth = screenWidth - (base.screenGapLeft + base.screenGapRight + base.frameBorderWidth);
+    const windowInnerWidth =
+      screenWidth - (base.screenGapLeft + base.screenGapRight + base.frameBorderWidth);
     const windowWidth = windowInnerWidth + windowFraming.frameBorderWidth;
 
-    const windowInnerHeight = screenHeight - (base.screenGapTop + base.screenGapBottom + base.frameBorderHeight);
+    const windowInnerHeight =
+      screenHeight - (base.screenGapTop + base.screenGapBottom + base.frameBorderHeight);
     const windowHeight = windowInnerHeight + windowFraming.frameBorderHeight;
+
+    const availableScreenWidth =
+      screenWidth - (windowFraming.screenGapLeft + windowFraming.screenGapRight);
+    const leftoverSpacingWidth = availableScreenWidth - windowWidth;
+    const positionX = randomIntFromInterval(
+      windowFraming.screenGapLeft,
+      windowFraming.screenGapLeft + leftoverSpacingWidth,
+    );
+
+    const availableScreenHeight =
+      screenHeight - (windowFraming.screenGapTop + windowFraming.screenGapBottom);
+    const leftoverSpacingHeight = availableScreenHeight - windowHeight;
+    const positionY = randomIntFromInterval(
+      windowFraming.screenGapTop,
+      windowFraming.screenGapTop + leftoverSpacingHeight,
+    );
 
     return {
       positionX,
@@ -25,4 +40,11 @@ export default class Viewports {
       deviceScaleFactor: 1,
     } as IViewport;
   }
+}
+
+// HELPERS
+
+function randomIntFromInterval(min, max) {
+  if (min === max) return min;
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
