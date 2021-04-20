3e6ffaf52cdf2933a5608417ee7178ed36512aa8
blakebyrnes <blakebyrnes@gmail.com>
2021-04-20 18:03:59 -0400

    fix(deps): peg awaited dom version

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,18 @@
 const fs = require('fs');
 
-module.exports = () => {
+// eslint-disable-next-line import/no-extraneous-dependencies
+const CertificateManager = require('@secret-agent/mitm-socket/lib/CertificateGenerator').default;
+
+module.exports = async () => {
   try {
     fs.rmdirSync(`${__dirname}/.sessions-test`, { recursive: true });
+    fs.mkdirSync(`${__dirname}/.sessions-test`);
+    // generate certs
+    const certManager = new CertificateManager({
+      storageDir: `${__dirname}/.sessions-test`,
+    });
+    await certManager.waitForConnected;
+    certManager.close();
   } catch (err) {
     // ignore
   }
