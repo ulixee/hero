7447d68342aedf6a5e85cce5d721d4d4d5795b1e
Caleb Clark <calebjclark@gmail.com>
2021-07-28 21:02:48 -0500

    fix: removed unussed core/connector.ts + fixed workspace issue

--- core/connector.ts
+++
@@ -1,6 +0,0 @@
-import InjectedScripts from './lib/InjectedScripts';
-import Core, { GlobalPool } from './index';
-import SessionDb from './dbs/SessionDb';
-import * as pkg from './package.json';
-
-export { pkg, InjectedScripts, Core, GlobalPool, SessionDb };
