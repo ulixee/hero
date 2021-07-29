1486bb931d6bbbcf8b3227d07e7b2d729f484d9b
Caleb Clark <calebjclark@gmail.com>
2021-07-28 20:45:23 -0500

    fix: removed ConnectionToReplay

--- core/connector.ts
+++ core/connector.ts
@@ -1,7 +1,6 @@
-import ConnectionToReplay from './connections/ConnectionToReplay';
 import InjectedScripts from './lib/InjectedScripts';
 import Core, { GlobalPool } from './index';
 import SessionDb from './dbs/SessionDb';
 import * as pkg from './package.json';
 
-export { pkg, ConnectionToReplay, InjectedScripts, Core, GlobalPool, SessionDb };
+export { pkg, InjectedScripts, Core, GlobalPool, SessionDb };
