de381db8c65f2ac76c1eec8c59fc8ff3332c09f5
blakebyrnes <blakebyrnes@gmail.com>
2021-08-19 17:47:00 -0400

    chore: cleanup tsconfigs

--- core/injected-scripts/.eslintrc.js
+++ core/injected-scripts/.eslintrc.js
@@ -3,6 +3,6 @@ const { join } = require('path');
 module.exports = {
   extends: '../../.eslintrc.js',
   parserOptions: {
-    project: join(__dirname, '/tsconfig.json'),
+    project: join(__dirname, 'tsconfig.json'),
   },
 };
