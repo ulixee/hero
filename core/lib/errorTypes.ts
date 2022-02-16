c4d95b26f73460bddacaccdcfa9584f7da000835
Blake Byrnes <blakebyrnes@gmail.com>
2022-02-16 13:19:05 -0500

    fix(core): improve script changed message

--- core/lib/errorTypes.ts
+++ core/lib/errorTypes.ts
@@ -1,5 +1,6 @@
 import { registerSerializableErrorType } from '@ulixee/commons/lib/TypeSerializer';
 import ICommandMeta from '@ulixee/hero-interfaces/ICommandMeta';
+import SourceLoader from '@ulixee/commons/lib/SourceLoader';
 
 export class ScriptChangedNeedsRestartError extends Error {
   constructor(message?: string) {
@@ -9,11 +10,12 @@ export class ScriptChangedNeedsRestartError extends Error {
   }
 
   public static atCommand(command: ICommandMeta): ScriptChangedNeedsRestartError {
-    const message = `Your script has changed. 
-    
-  >> script is changed at: "${command.callsite}"
-    
-To resume this session, your script needs to be restarted from the start.`;
+    let scriptLocation = '';
+    if (command.callsite) {
+      const source = SourceLoader.getSource(command.callsite[0]);
+      if (source?.code) scriptLocation = `\n\n>> script is changed at line #${source.line}. "${source.code.trim()}\n\n\n"`;
+    }
+    const message = `Your script has changed. ${scriptLocation}To resume this session, your script needs to be restarted from the start.`;
     return new ScriptChangedNeedsRestartError(message);
   }
 }
