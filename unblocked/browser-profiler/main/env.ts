5e91c401211508c5328b5a483222e5718d7b9f5b
Blake Byrnes <blakebyrnes@gmail.com>
2022-10-28 13:36:43 -0400

    fix: update env parsing of paths

--- unblocked/browser-profiler/main/env.ts
+++ unblocked/browser-profiler/main/env.ts
@@ -1,19 +1,10 @@
 import '@ulixee/commons/lib/SourceMapSupport';
 import { loadEnv } from '@ulixee/commons/lib/envUtils';
 import * as Path from 'path';
-import * as Os from 'os';
 
-loadEnv(Path.resolve(__dirname,'..'));
+loadEnv(Path.resolve(__dirname, '..'));
 const { env } = process;
-
 export default {
   browserStackUser: env.BROWSERSTACK_USER,
   browserStackKey: env.BROWSERSTACK_ACCESS_KEY,
 };
-
-export function parseEnvPath(envvar: string, relativeTo?: string): string {
-  if (!envvar) return null;
-  if (envvar?.startsWith('~')) envvar = Path.join(Os.homedir(), envvar.slice(1));
-  if (Path.isAbsolute(envvar)) return envvar;
-  return Path.resolve(relativeTo ?? process.cwd(), envvar);
-}
