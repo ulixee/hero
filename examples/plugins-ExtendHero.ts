b789b0e392dd6e25210e95cbbfc0aac26d7062e2
glendc <glen.decauwsemaecker@otainsight.com>
2022-11-08 19:27:40 +0100

    feat(example): showcase how to extend hero with your own function
    where your custom function can build on the
    rich features offered by the hero instance itself
    
    this is for example useful to make advanced functions
    that allow you to solve captchas or other
    complex repetitive work that you may wish to
    use between your different hero scripts

---
+++ examples/plugins-ExtendHero.ts
@@ -0,0 +1,31 @@
+import Hero from '@ulixee/hero';
+import { ClientPlugin } from '@ulixee/hero-plugin-utils';
+
+// NOTE: You need to start a Ulixee Miner to run this example
+
+export default class HeroExtensionPlugin extends ClientPlugin {
+  static override readonly id = 'hero-extension-plugin';
+
+  onHero(hero: Hero) {
+    // define your own hero function,
+    // which internally can make use of the actual `Hero` instance,
+    // by including it in your own clojure-as-a-method!
+    (hero as any).revealAnswers = async (selector?: string) =>
+      await hero
+        .querySelectorAll(selector ?? 'li.question')
+        .$map((link) => link.$click());
+  }
+}
+
+(async function main() {
+  const hero = new Hero();
+  hero.use(HeroExtensionPlugin);
+
+  await hero.goto('https://cdpn.io/alexs/fullpage/AJGEWY');
+  await hero.activeTab.waitForPaintingStable();
+
+  // you now have the ability to use your own custom function :)
+  await hero.revealAnswers();
+
+  await hero.close();
+})();
