64e748f0b15c9299f75b9ecbbbd595408c56d62e
Blake Byrnes <blakebyrnes@gmail.com>
2022-11-15 09:59:37 -0500

    fix: example interface

--- examples/plugins-ExtendHero.ts
+++ examples/plugins-ExtendHero.ts
@@ -5,7 +5,7 @@ import { ClientPlugin } from '@ulixee/hero-plugin-utils';
 
 declare module '@ulixee/hero/lib/extendables' {
   interface Hero {
-    revealAnswers();
+    revealAnswers(selector?: string): Promise<any>;
   }
 }
 export default class HeroExtensionPlugin extends ClientPlugin {
