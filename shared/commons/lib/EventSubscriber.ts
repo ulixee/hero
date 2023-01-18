0d4b350e8b358c0fa84db7771867deb6f4d30f91
Blake Byrnes <blakebyrnes@gmail.com>
2023-01-18 16:12:30 -0500

    fix(commons): remove memory for once events

--- shared/commons/lib/EventSubscriber.ts
+++ shared/commons/lib/EventSubscriber.ts
@@ -2,9 +2,10 @@ import IRegisteredEventListener, { IEventSubscriber } from '../interfaces/IRegis
 
 type AnyFunction = (...args: any[]) => any;
 
+let idCounter = 0;
 export default class EventSubscriber implements IEventSubscriber {
   public groups: { [key: string]: IRegisteredEventListener[] } = {};
-  private readonly registeredEventListeners = new Set<IRegisteredEventListener>();
+  private readonly registeredEventListeners = new Set<IRegisteredEventListener & { id: number }>();
 
   on<K extends string | symbol, Func extends AnyFunction>(
     emitter: {
@@ -20,7 +21,8 @@ export default class EventSubscriber implements IEventSubscriber {
     includeUnhandledEvents?: boolean,
   ): IRegisteredEventListener {
     emitter.on(eventName, handler, includeUnhandledEvents);
-    const registeredEvent: IRegisteredEventListener = { emitter, eventName, handler };
+    const id = idCounter++;
+    const registeredEvent = { id, emitter, eventName, handler };
     this.registeredEventListeners.add(registeredEvent);
     return registeredEvent;
   }
@@ -38,16 +40,22 @@ export default class EventSubscriber implements IEventSubscriber {
     handler: (...args: Parameters<Func>) => ReturnType<Func>,
     includeUnhandledEvents?: boolean,
   ): IRegisteredEventListener {
-    emitter.once(eventName, handler, includeUnhandledEvents);
-    const registeredEvent: IRegisteredEventListener = { emitter, eventName, handler };
+    const id = idCounter++;
+    const finalHandler = (...args: Parameters<Func>): ReturnType<Func> => {
+      this.removeHandlerById(id);
+      return handler(...args);
+    };
+    const registeredEvent = { id, emitter, eventName, handler: finalHandler };
+    emitter.once(eventName, finalHandler, includeUnhandledEvents);
     this.registeredEventListeners.add(registeredEvent);
     return registeredEvent;
   }
 
   off(...listeners: IRegisteredEventListener[]): void {
     for (const listener of listeners) {
+      if (!listener.emitter) continue;
       listener.emitter.off(listener.eventName, listener.handler);
-      this.registeredEventListeners.delete(listener);
+      this.registeredEventListeners.delete(listener as any);
     }
     listeners.length = 0;
   }
@@ -72,5 +80,14 @@ export default class EventSubscriber implements IEventSubscriber {
     const events = this.groups[name];
     delete this.groups[name];
     if (events) this.off(...events);
+  }
+
+  private removeHandlerById(id: number): void {
+    for (const listener of this.registeredEventListeners) {
+      if (listener.id === id) {
+        this.registeredEventListeners.delete(listener);
+        break;
+      }
+    }
   }
 }
