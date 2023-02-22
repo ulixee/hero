c12aa36bdb4c9e1dbe2965693cb2f7917e8ba526
Blake Byrnes <blakebyrnes@gmail.com>
2023-02-22 13:54:22 -0500

    chore: remove datastore apis from spec

--- shared/specification/datastore/index.ts
+++
@@ -1,8 +0,0 @@
-import { IZodHandlers, IZodSchemaToApiTypes } from '../utils/IZodApi';
-import { DatastoreApiSchemas } from './DatastoreApis';
-
-export type IDatastoreApiTypes = IZodSchemaToApiTypes<typeof DatastoreApiSchemas>;
-
-export type IDatastoreApis = IZodHandlers<typeof DatastoreApiSchemas>;
-
-export default DatastoreApiSchemas;
--- shared/specification/types/IMicronote.ts
+++ shared/specification/types/IMicronote.ts
@@ -22,28 +22,6 @@ export const MicronoteSchema = z.object({
   guaranteeBlockHeight: blockHeightValidation,
 });
 
-export const PaymentSchema = z.object({
-  micronote: MicronoteSchema.extend({
-    holdAuthorizationCode: z
-      .string()
-      .length(16)
-      .optional()
-      .describe('A hold authorization code granting sub-holds on a micronote.'),
-  }).optional(),
-  credits: z
-    .object({
-      id: z
-        .string()
-        .length(11)
-        .regex(
-          /^crd[A-Za-z0-9_]{8}$/,
-          'This is not a Datastore credits id (starting with "cred", following by 8 alphanumeric characters).',
-        ),
-      secret: z.string().length(12),
-    })
-    .optional(),
-});
+type IMicronote = z.infer<typeof MicronoteSchema>;
 
-type IPayment = z.infer<typeof PaymentSchema>;
-
-export default IPayment;
+export default IMicronote;
