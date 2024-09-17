3e2855844c0be2350b5239172b1aa92a699b258b
Blake Byrnes <blakebyrnes@gmail.com>
2024-09-17 09:29:13 -0400

    fix(ghactions): js branch publishing go

--- agent/mitm-socket/install.js
+++ agent/mitm-socket/install.js
@@ -23,22 +23,26 @@ const forceBuild = Boolean(JSON.parse(process.env.ULX_MITM_REBUILD_SOCKET || 'fa
     programName += '.exe';
   }
 
-  if (fs.existsSync(`${__dirname}/dist/artifacts.json`)) {
-    const artifacts = JSON.parse(fs.readFileSync(`${__dirname}/dist/artifacts.json`, 'utf8'));
-    let platform = os.platform();
-    if (os.platform() === 'win32') {
-      platform = 'windows';
+  try {
+    if (fs.existsSync(`${__dirname}/dist/artifacts.json`)) {
+      const artifacts = JSON.parse(fs.readFileSync(`${__dirname}/dist/artifacts.json`, 'utf8'));
+      let platform = os.platform();
+      if (os.platform() === 'win32') {
+        platform = 'windows';
+      }
+      let arch = os.arch();
+      if (os.arch() === 'x86_64') {
+        arch = 'amd64';
+      }
+      const artifact = artifacts.find(x => x.goos === platform && x.goarch === arch);
+      if (artifact) {
+        fs.copyFileSync(artifact.path, `${__dirname}/dist/${artifact.name}`);
+        saveVersion();
+        console.log('Successfully copied Agent connect library');
+      }
     }
-    let arch = os.arch();
-    if (os.arch() === 'x86_64') {
-      arch = 'amd64';
-    }
-    const artifact = artifacts.find(x => x.goos === platform && x.goarch === arch);
-    if (artifact) {
-      fs.copyFileSync(artifact.path, `${__dirname}/dist/${artifact.name}`);
-      saveVersion();
-      console.log('Successfully copied Agent connect library');
-    }
+  } catch (err) {
+    console.log('Error copying Agent connect library from prebuilds', err);
   }
 
   const installed = getInstalledVersion();
