94f3a52f3f332dbf6a0302c0cfce0d71e9e72a6f
brecht stamper <stamper.brecht@gmail.com>
2024-10-07 17:36:54 +0200

    chore: support local path overrides

--- plugins/default-browser-emulator/paths.ts
+++ plugins/default-browser-emulator/paths.ts
@@ -1,4 +1,17 @@
 import * as Path from 'path';
 import * as Paths from './paths.json';
 
-export const emulatorDataDir = Path.resolve(__dirname, Paths['emulator-data']);
+let PathsLocal;
+try {
+  // eslint-disable-next-line import/no-unresolved
+  PathsLocal = require('./paths.local.json');
+} catch {
+  PathsLocal = {};
+}
+
+const PathsMerged = {
+  ...Paths,
+  ...PathsLocal,
+};
+
+export const emulatorDataDir = Path.resolve(__dirname, PathsMerged['emulator-data']);
